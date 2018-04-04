5922696812b48fde820b189b32cbc9ee2f21a23f
Ben Flores <benjaminhyw@gmail.com>
2018-04-03 20:52:20 -0700

    * cracking_the_coding_interview_CSharp.csproj: Initial commit
    * Program.cs:
    * AssemblyInfo.cs:

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cracking_the_coding_interview_CSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
